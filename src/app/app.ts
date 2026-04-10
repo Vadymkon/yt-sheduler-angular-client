a8b05ff6e9ed43027d85d899c3cb8044cdcb2488
VadymKon <vadykakonon@gmail.com>
2026-04-10 19:49:45 +0300

    Mat-side-nav template.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ModeSwitcherComponent } from './components/mode-switcher.component/mode-switcher.component';
 import { WorkspacePage } from './pages/workspace.page/workspace.page';
+import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WorkspacePage],
+  imports: [RouterOutlet, WorkspacePage, MatSidenavContent, MatSidenav, MatSidenavContainer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
