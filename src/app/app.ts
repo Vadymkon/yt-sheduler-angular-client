53a5a1b953cc6630a686e8f472fe208897722a86
VadymKon <vadykakonon@gmail.com>
2026-04-11 07:53:54 +0300

    Mat-side-nav plain code. Works.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WorkspacePage } from './pages/workspace.page/workspace.page';
-import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
+import { WhatDeviceService } from './services/what-device.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WorkspacePage, MatSidenavContent, MatSidenav, MatSidenavContainer],
+  imports: [WorkspacePage, MatSidenavModule, MatIcon, MatIconButton, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('yt-sheduler-angular');
+  whatDeviceService = inject(WhatDeviceService);
 }
